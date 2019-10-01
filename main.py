47ba0686d47ad45529ee82ca84a1de70576a211f
ized <30953120+izedout@users.noreply.github.com>
2019-10-01 10:15:08 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
+import requests
 
+page = requests.get("https://livestreamfails.com/top")
+links = page.text
+
+# regex to get only links in the page
+# remove unwanted links (nav bar etc)
+# extract links and place into a list
+# iterate over links
