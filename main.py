72cc8e064c026668ce84754b85e32e8ba98edea5
ized <30953120+izedout@users.noreply.github.com>
2019-10-11 00:49:37 +0800

    More verbose CLI, better comments, condensed ffmpeg calls, managed intrim files

--- main.py
+++ main.py
@@ -9,11 +9,19 @@ import re
 import os
 import subprocess
 
+print("<------------------>")
+print("LSF CONTENT BUILDER")
+print("by @__ized - github.com/izedout")
+print("")
+print("PagChomp")
+print("<------------------>")
+print("")
+
 # define runtime variables
 fadetime = 24*3 #3 seconds
 fadein = fadetime
 
-# funtion to get duration of clip for use later
+# funtion to get duration of clip in frames
 def getLength(filename):
     result = subprocess.Popen(["ffprobe", "-show_streams", filename],
         stdout = subprocess.PIPE, stderr = subprocess.STDOUT)
@@ -22,10 +30,13 @@ def getLength(filename):
     clean = re.findall(r'\d+', str(dirty))
     return int(clean[0])
 
+print("GETTING CLIPS:")
 url = "https://livestreamfails.com/top"
+print("Navigating to " + url + "!")
 browser = webdriver.Chrome()
 browser.get(url)
-delay = 5 #seconds
+delay = 5 #seconds max loading time before being considered timed out
+print("Filtering for posts made in the past 24h!")
 browser.execute_script("loadPostTimeFrameSelect(this, 'day')") # uses js to filter posts from the last 24h
 
 try:
@@ -38,18 +49,23 @@ html = browser.page_source
 soup = BeautifulSoup(html, 'lxml')
 browser.close()
 
+print("Parsing HTML!")
+# Gets the links of all the posts on the current page
 links = []
 for link in soup.find_all("a", attrs={"href": re.compile("^https://livestreamfails.com/post/")}):
     links.append(link.get("href"))
-
 links = list(set(links)) # removes duplicate links (bcause there are 3 different links for each post; picture, title etc)
-print(links)
-# links is now an array/list of all the links to the top posts of the past 24 hours
+print("Here are all of the top posts from the past 24h:" + str(links)) # links is now an array/list of all the links to the top posts of the past 24 hours
+print("<----------------------->")
+print("")
+
 videourls = []
 titles = []
 clips = 0
 
+print("ENUMERATING CLIPS:")
 for link in links:
+    print("Navigating to clip" + str(clips) + "!")
     browser = webdriver.Chrome()
     browser.get(link)
     delay = 5 #seconds
@@ -65,50 +81,56 @@ for link in links:
     soup = BeautifulSoup(html, 'lxml')
     browser.close()
 
-    links = []
+    print("Parsing clip" + str(clips) + "!")
     for link in soup.find_all("source", attrs={"src": re.compile("^https://stream.livestreamfails.com/video/")}):
         videourls.append(link.get("src"))
 
-    titles = []
     for link in soup.find_all("h4",{"class":"post-title"}):
         titles.append(link.text)
 
-    print(videourls)
-    print(titles)
-    clips = clips + 1
-    # break loop when total video time is greater than 5 minutes
+    clips += 1
+    # break loop when the number of clips is 2
     if clips > 1:
         break
-print("-------------------")
+
+print("<------------------->")
 print("All video urls:")
 print(videourls)
-print("-------------------")
+print("All video titles:")
+print(titles)
+print("<------------------->")
+print("")
 
 # Downloads and preprocesses all clips
+print("DOWNLOADING + PROCESSING CLIPS!")
 for i in range(len(videourls)):
+    # have to create additional files for ffmpeg to operate 
     clipname = "clip" + str(i) + ".mp4"
     outname = "out" + str(i) + ".mp4"
 
     #download clip
+    print("Downloading clip" + str(i) + "!")
     os.system("curl " + videourls[i] + " -o " + clipname)
 
     # all files will be made into 1080p, 24fps format to prevent any issues later on
-    os.system("ffmpeg -i " + clipname + " -vf scale=1920:1080,fps=fps=24 " + outname)
+    # this has to be done seperate from fades, because fadeout transition relies on number of frames, which changes
+    os.system("ffmpeg -i " + clipname + " -y -vf scale=1920:1080,fps=fps=24 " + outname)
+    os.system("del " + clipname)
 
-    # Get file duration in frames
-    frames = getLength(outname)
-    print("")
-    print(frames)
-    print("")
-    fadeout = frames - fadetime
-    print(fadeout)
-    print("")
-    fadename = "faded" + str(i) + ".ts"
-    os.system("ffmpeg -i " + outname + """ -vf "fade=in:0:""" + str(fadein) + """" 1""" + outname)
-    os.system("ffmpeg -i 1" + outname + """ -vf "fade=out:"""+ str(fadeout) + ":" + str(fadetime) + """" 2""" + outname)
-    os.system("ffmpeg -i 2" + outname + " " + fadename)
+    frames = getLength(outname)     # Get file duration in frames
+    fadeout = frames - fadetime     # Gets the time in frames when the fadeout transition should begin
 
+    # adds in and out fades and transcodes to MPEG2 simultaneously
+    os.system("ffmpeg -i " + outname + """ -y -vf "fade=in:0:""" + str(fadein) + ",fade=out:"+ str(fadeout) + ":" + str(fadetime) + """" """ + str(i) + ".ts")
+    os.system("del " + outname)
 
+print("<----------------------->")
+print("")
+
+print("RUNNING FINAL CONCAT AND TRANSCODE ON CLIPS!")
 # combines all faded files together and transcodes them back to mp4
-os.system("""ffmpeg -i "concat:faded0.ts|faded1.ts" -c copy final.ts""")
+os.system("""ffmpeg -i "concat:0.ts|1.ts" -c copy final.ts""")
+os.system("del 0.ts && del 1.ts")
 os.system("ffmpeg -i final.ts final.mp4")
+os.system("del final.ts")
+print("Done!")
