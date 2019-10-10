15b66badf680896673d8554e4d62a47e01700762
ized <30953120+izedout@users.noreply.github.com>
2019-10-10 18:47:25 +0800

    Working, no audio, have to abandon ffmpeg-concat

--- main.py
+++ main.py
@@ -17,12 +17,12 @@ browser.execute_script("loadPostTimeFrameSelect(this, 'day')") # uses js to filt
 try:
     myElem = WebDriverWait(browser, delay).until(EC.presence_of_element_located((By.CLASS_NAME, 'card-deck')))
     print("Page is ready!")
-
 except TimeoutException:
     print("Page took too long to load!")
 
 html = browser.page_source
 soup = BeautifulSoup(html, 'lxml')
+browser.close()
 
 links = []
 for link in soup.find_all("a", attrs={"href": re.compile("^https://livestreamfails.com/post/")}):
@@ -31,12 +31,14 @@ for link in soup.find_all("a", attrs={"href": re.compile("^https://livestreamfai
 links = list(set(links)) # removes duplicate links (bcause there are 3 different links for each post; picture, title etc)
 print(links)
 # links is now an array/list of all the links to the top posts of the past 24 hours
+videourls = []
+titles = []
+clips = 0
 
 for link in links:
     browser = webdriver.Chrome()
     browser.get(link)
     delay = 5 #seconds
-    clips = 0
 
     try:
         myElem = WebDriverWait(browser, delay).until(EC.presence_of_element_located((By.CLASS_NAME, 'post-title')))
@@ -47,27 +49,34 @@ for link in links:
 
     html = browser.page_source
     soup = BeautifulSoup(html, 'lxml')
+    browser.close()
 
     links = []
     for link in soup.find_all("source", attrs={"src": re.compile("^https://stream.livestreamfails.com/video/")}):
-        links.append(link.get("src"))
+        videourls.append(link.get("src"))
 
     titles = []
     for link in soup.find_all("h4",{"class":"post-title"}):
         titles.append(link.text)
 
-    print(links)
+    print(videourls)
     print(titles)
     clips = clips + 1
     # break loop when total video time is greater than 5 minutes
-    if clips > 14:
+    if clips > 1:
         break
 
-for i in links:
-	os.system("curl " + links[i] + " -o clip" + i)
-	os.system("ffmpeg -i clip" + i + ".mp4 -vf scale=1920:1080 out" + i + ".ts")
+print("All video urls:")
+print(videourls)
+
+for i in range(len(videourls)):
+    os.system("curl " + videourls[i] + " -o clip" + str(i) +".mp4")
+    os.system("ffmpeg -i clip" + i + ".mp4 -vf scale=1920:1080 out" + i + ".ts")
 
 
 #used ffmpeg to cocant 15 MPEG-2 files losslessly into a single MPEG-2 output file, which must then be reencoded into mp4
-os.system("""ffmpeg -i "concat:out1.ts|out2.ts|out3.ts|out4.ts|out5.ts|out6.ts|out7.ts|out8.ts|out9.ts|out10.ts|out11.ts|out12.ts|out13.ts|out14.ts|out15.ts" -c copy final.ts""")
-os.system("ffmpeg -i final.ts -o final.mp4")
+#os.system("""ffmpeg -i "concat:out1.ts|out2.ts|out3.ts|out4.ts|out5.ts|out6.ts|out7.ts|out8.ts|out9.ts|out10.ts|out11.ts|out12.ts|out13.ts|out14.ts|out15.ts" -c copy final.ts""")
+#os.system("ffmpeg -i final.ts -o final.mp4")
+os.system("ffmpeg-concat -t circleopen -d 100 -f jpg -o final.mp4 clip0.mp4 clip1.mp4")
+
+
