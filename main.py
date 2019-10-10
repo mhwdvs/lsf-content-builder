e9d0fef510a1fce3f4e6b4231889c46d927f3d1f
ized <30953120+izedout@users.noreply.github.com>
2019-10-10 09:52:55 +0800

    Update main.py

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.common.by import By
 import lxml
 import re
+import os
 
 url = "https://livestreamfails.com/top"
 browser = webdriver.Chrome()
@@ -22,8 +23,9 @@ except TimeoutException:
 
 html = browser.page_source
 soup = BeautifulSoup(html, 'lxml')
-
 links = []
+browser.quit()
+
 for link in soup.find_all("a", attrs={"href": re.compile("^https://livestreamfails.com/post/")}):
     links.append(link.get("href"))
 
@@ -31,11 +33,13 @@ links = list(set(links)) # removes duplicate links (bcause there are 3 different
 print(links)
 # links is now an array/list of all the links to the top posts of the past 24 hours
 
+clips = 0
+
+
 for link in links:
     browser = webdriver.Chrome()
     browser.get(link)
     delay = 5 #seconds
-    time = 2
 
     try:
         myElem = WebDriverWait(browser, delay).until(EC.presence_of_element_located((By.CLASS_NAME, 'post-title')))
@@ -46,8 +50,9 @@ for link in links:
 
     html = browser.page_source
     soup = BeautifulSoup(html, 'lxml')
-
     links = []
+    browser.quit()
+    
     for link in soup.find_all("source", attrs={"src": re.compile("^https://stream.livestreamfails.com/video/")}):
         links.append(link.get("src"))
 
@@ -57,8 +62,20 @@ for link in links:
 
     print(links)
     print(titles)
-
-
+    clips += 1
     # break loop when total video time is greater than 5 minutes
-    if time > 5:
+    if clips > 2:
         break
+
+print(links)
+print(links[0])
+linknumber = 0
+
+for i in links:
+    os.system("curl " + i + " -o clip" + str(linknumber) + ".mp4" )
+    os.system("ffmpeg -i clip" + str(linknumber) + ".mp4 -vf scale=1920:1080 out" + str(linknumber) + ".ts")
+
+
+#used ffmpeg to cocant 15 MPEG-2 files losslessly into a single MPEG-2 output file, which must then be reencoded into mp4
+os.system("""ffmpeg -i "concat:out0.ts|out1.ts|out2.ts" -c copy final.ts""")
+os.system("ffmpeg -i final.ts -o final.mp4")
